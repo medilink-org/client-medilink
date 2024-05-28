8c6cc4f816cb1d3071135dad793a4129df396fdd
aimenabdulaziz <aimenabdulaziz@users.noreply.github.com>
2024-05-27 22:00:13 -0400

    feat: add link to Users page

--- src/components/Layout/AdminHome.tsx
+++ src/components/Layout/AdminHome.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import UserPage from '../Admin/ManageUsers/UsersPage';
+import { Button } from '@mui/material';
 
 export default function AdminHome() {
   return (
     <div>
       <h1>Admin Home...</h1>
+      <Button>
+        <Link to="/users">Go to User Page</Link>
+      </Button>
     </div>
   );
 }
