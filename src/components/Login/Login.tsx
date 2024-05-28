4874d9099c28af4454d38c2b68bd57395d92dd59
Sajjad C Kareem <sajjadck04@gmail.com>
2024-05-27 22:09:28 -0400

    refactor: refactored Login.tsx to remove console.log statements (security issue)

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -27,6 +27,7 @@ export default function Login() {
     }
 
     try {
+      console.log('Logging in with:', username, password);
       const user = await login({ username, password }).unwrap();
       console.log('Logged in user:', user);
 
