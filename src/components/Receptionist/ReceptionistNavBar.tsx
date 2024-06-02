b7250fdeb875b94bb26c7e5b6ebdb6c2bad125af
Caleb Ash <124649246+calebashw@users.noreply.github.com>
2024-06-01 21:15:57 -0400

    Fix: Swapping out deprecated code

--- src/components/Receptionist/ReceptionistNavBar.tsx
+++ src/components/Receptionist/ReceptionistNavBar.tsx
@@ -14,19 +14,63 @@ import './style/ReceptionistHome.css';
 const { Sider } = Layout;
 
 const ReceptionistNav = () => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const showModal = () => {
-    setIsModalVisible(true);
-  };
-
-  const handleOk = () => {
-    setIsModalVisible(false);
+    setIsModalOpen(true);
   };
 
   const handleCancel = () => {
-    setIsModalVisible(false);
+    setIsModalOpen(false);
   };
+
+  const menuItems = [
+    {
+      key: '1',
+      icon: <UserAddOutlined />,
+      label: (
+        <Button type="link" onClick={showModal} style={{ color: 'white' }}>
+          Check In Patients
+        </Button>
+      )
+    },
+    {
+      key: '2',
+      icon: <UserSwitchOutlined />,
+      label: (
+        <Link to="/assign-patient" style={{ color: 'white' }}>
+          Assign to Doctor
+        </Link>
+      )
+    },
+    {
+      key: '3',
+      icon: <UserOutlined />,
+      label: (
+        <Link to="/view-patients" style={{ color: 'white' }}>
+          View Patient List
+        </Link>
+      )
+    },
+    {
+      key: '4',
+      icon: <UsergroupAddOutlined />,
+      label: (
+        <Link to="/doctor-page" style={{ color: 'white' }}>
+          Doctor page
+        </Link>
+      )
+    },
+    {
+      key: '5',
+      icon: <QrcodeOutlined />,
+      label: (
+        <Link to="/generate-qrcode" style={{ color: 'white' }}>
+          QR Code
+        </Link>
+      )
+    }
+  ];
 
   return (
     <Sider collapsible>
@@ -37,37 +81,15 @@ const ReceptionistNav = () => {
           style={{ width: '100%' }}
         />
       </div>
-      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-        <Menu.Item key="1" icon={<UserAddOutlined />}>
-          <Button type="link" onClick={showModal} style={{ color: 'white' }}>
-            Check In Patients
-          </Button>
-        </Menu.Item>
-        <Menu.Item key="2" icon={<UserSwitchOutlined />}>
-          <Link to="/assign-patient" style={{ color: 'white' }}>
-            Assign to Doctor
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="3" icon={<UserOutlined />}>
-          <Link to="/view-patients" style={{ color: 'white' }}>
-            View Patient List
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="4" icon={<UsergroupAddOutlined />}>
-          <Link to="/doctor-page" style={{ color: 'white' }}>
-            Doctor page
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="4" icon={<QrcodeOutlined />}>
-          <Link to="/generate-qrcode" style={{ color: 'white' }}>
-            QR Code
-          </Link>
-        </Menu.Item>
-      </Menu>
+      <Menu
+        theme="dark"
+        defaultSelectedKeys={['1']}
+        mode="inline"
+        items={menuItems}
+      />
       <Modal
         title="Check-In Patient"
-        visible={isModalVisible}
-        onOk={handleOk}
+        open={isModalOpen}
         onCancel={handleCancel}
         footer={null}>
         <CheckInForm />
